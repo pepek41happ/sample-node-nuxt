c2b1f3d3ac00555d66b71b0a31625c3c5121e828
Thomas Wickham <mackwic@gmail.com>
2021-02-25 12:23:14 +0100

    Integrate Scalingo Markup

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,301 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        sample-nuxt-scalingo
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+  <div class="root">
+    <div class="container hero">
+      <div class="love">
+        <img
+          class="scalingo-logo"
+          alt="Scalingo"
+          src="https://scalingo.com/logo-lightgrey.svg"
+        />
+        <img
+          class="heart-logo"
+          alt="love"
+          src="https://cdn.scalingo.com/documentation/heart.png"
+        />
+        <svg
+          xmlns:dc="http://purl.org/dc/elements/1.1/"
+          xmlns:cc="http://creativecommons.org/ns#"
+          xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+          xmlns:svg="http://www.w3.org/2000/svg"
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          width="243"
+          height="213"
+          viewBox="0 0 243 213"
+          id="svg2"
+          class="tech-logo"
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+          <metadata id="metadata34">
+            <rdf:RDF>
+              <cc:Work rdf:about="">
+                <dc:format>image/svg+xml</dc:format>
+                <dc:type
+                  rdf:resource="http://purl.org/dc/dcmitype/StillImage"
+                />
+                <dc:title></dc:title>
+              </cc:Work>
+            </rdf:RDF>
+          </metadata>
+          <defs id="defs32" />
+          <g id="g8" style="fill:#a90533">
+            <path
+              d="M 38.003327,153.65612 C 19.2,133.73121 8.1983366,106.20487 8.2262178,73.362099 8.2002376,44.316871 17.326257,19.351168 33.015129,0.35394059 l -0.02535,0.009505 -32.97584141,0 0,153.29584441 37.96594041,0 0.02345,-0.003 0,0 z"
+              id="path10"
+            />
+            <path
+              d="m 211.67271,153.66879 c 8.10012,-8.67548 15.3581,-18.89267 21.93489,-30.57742 l -36.49901,-18.24951 c -12.81837,20.83929 -31.56404,40.02091 -57.48594,40.14891 -37.72641,-0.128 -52.549066,-32.3878 -52.467323,-73.910493 l 140.977423,0 c 0.1882,-4.687841 0.1882,-6.867643 0,-9.124119 0.92008,-24.703366 -5.62693,-45.467881 -17.53663,-61.63833622 l -0.0615,0.0462574 31.74147,0 0,153.29584082 -30.67248,0 0.0691,0.009 0,0 z"
+              id="path12"
+            />
+            <path
+              d="M 89.437149,35.493743 C 90.997861,16.694218 105.81988,4.0513663 123.19873,4.0133465 c 17.49735,0.03802 30.13957,12.6808715 30.56792,31.4803965 l -64.329501,0 0,0 z"
+              id="path14"
+            />
+          </g>
+          <g id="g16">
+            <path
+              d="m 20.08903,179.6084 0,-6.386693 -19.61821812,0 0,37.867723 19.61821812,0 0,-6.387961 -12.3184161,0 0,-10.036594 11.8615441,0 0,-6.387326 -11.8615441,0 0,-8.669149 12.3184161,0 0,0 z"
+              id="path18"
+            />
+            <path
+              d="m 53.394376,195.120475 c 4.890614,-0.72617 7.576079,-5.57243 7.75604,-10.49346 -0.179961,-8.05069 -5.398812,-11.38186 -12.774654,-11.40594 l -10.037227,0 0,37.8677227 7.299802,0 0,-15.055842 9.124752,15.055842 9.124753,0 -10.493466,-15.968317 0,0 z m -7.756673,-15.9676829 0.912475,0 c 3.982574,0.1495445 6.971565,1.0582178 6.843565,5.9310889 0.128,4.464792 -2.759604,5.676357 -6.843565,5.474852 l -0.912475,0 0,-11.4059409 0,0 z"
+              id="path20"
+            />
+            <path
+              d="m 86.699089,173.2217 -7.299168,0 0,37.867723 17.792633,0 0,-6.387961 -10.493465,0 0,-31.479762 0,0 z"
+              id="path22"
+            />
+            <path
+              d="m 134.60404,203.332753 3.19366,7.756039 7.75604,0 -14.14336,-38.78019794 -5.93109,0 -15.05584,38.78019794 7.7554,0 3.1943,-7.756039 13.23089,0 0,0 z m -1.82495,-5.930456 -9.12475,0 4.10614,-14.143366 5.01861,14.143366 0,0 z"
+              id="path24"
+            />
+            <path
+              d="m 159.2415,211.08943 7.75541,0 0,-25.549307 20.07509,26.461148 5.47422,0 0,-38.779564 -7.75541,0 0,25.549307 -20.07509,-26.46241594 -5.47422,0 0,38.78083194 0,0 z"
+              id="path26"
+            />
+            <path
+              d="m 224.48348,191.01434 0,5.931089 8.21228,0 c -0.17109,4.766416 -4.07192,8.804752 -8.66851,8.668515 -7.25925,0.136237 -10.85656,-6.87905 -10.94971,-13.230892 0.0932,-6.26693 3.63977,-13.5857421 10.94971,-13.6871282 3.8362,0.1007525 7.07928,2.7253861 8.66851,5.9310892 l 6.38733,-3.1936635 c -2.80903,-5.91715 -8.48349,-9.19826 -15.05584,-9.12476 -11.3128,-0.0729 -18.55747,9.2648 -18.70575,20.07446 0.14828,10.540357 7.19082,19.928713 18.24951,20.074456 11.94329,-0.145743 17.46566,-9.685545 17.33703,-20.530693 l 0,-0.912475 -16.42456,0 0,0 0,0 z"
+              id="path28"
+            />
+          </g>
+        </svg>
       </div>
+      <h1>Greetings from a sample Erlang app!</h1>
+      <h2>
+        <span style="display:block;"
+          ><span class="one-liner"
+            >Deploying your own Erlang app on Scalingo</span
+          >
+          is as easy as pie:</span
+        >
+        <a
+          class="btn"
+          href="https://my.osc-fr1.scalingo.com/deploy?source=https://github.com/Scalingo/sample-erlang"
+          >Just click this button!</a
+        >
+        <span style="display:block;"
+          >It will be up in less than 2 minutes!</span
+        >
+      </h2>
+      <p>
+        The code of this sample lives
+        <a href="https://github.com/Scalingo/sample-erlang">on GitHub</a>. You
+        can find <a href="https://samples.scalingo.com">more samples here</a>.
+      </p>
     </div>
+    <footer>
+      <div class="container">
+        <p>
+          <span class="one-liner"
+            ><a href="https://scalingo.com">Scalingo</a> is the best Platform as
+            a Service on the market:</span
+          >
+          no server to install, no dependencies to install, just push your code!
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({
+  head: {
+    title: "Greetings from a sample Erlang app!",
+    meta: [
+      { content: "website", property: "og:type" },
+      {
+        content:
+          "https://cdn.scalingo.com/homepage/assets/fb-33a6a93ddbf90bfdae57407481aa05a4.png",
+        property: "og:image"
+      },
+      {
+        content:
+          "Deploying your own Erlang app on Scalingo is as easy as pie: JUST CLICK THIS BUTTON! It will be up in less than 2 minutes!",
+        property: "og:description"
+      },
+      { charset: "utf-8" },
+      {
+        name: "viewport",
+        content: "width: device-width, initial-scale: 1, shrink-to-fit: no"
+      },
+      { httpEquiv: "x-ua-compatible", content: "ie=edge" }
+    ],
+    link: [
+      {
+        rel: "shortcut icon",
+        href: "https://scalingo.com/favicon.ico",
+        type: "image/vnd.microsoft.icon"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css"
+      },
+      {
+        href: "https://fonts.googleapis.com/css?family=Roboto:300,500",
+        rel: "stylesheet"
+      }
+    ]
+  }
+});
 </script>
 
 <style>
+body {
+  color: #373a3c;
+  font-family: "Roboto", sans-serif;
+  font-size: 1rem;
+  line-height: 1.5;
+  font-weight: 300;
+}
+.h1,
+.h2,
+.h3,
+.h4,
+.h5,
+.h6,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  color: inherit;
+  font-family: inherit;
+  font-weight: 500;
+  line-height: 1.1;
+  margin-bottom: 1rem;
+  margin-top: 0;
+}
+a {
+  color: #0275d8;
+  text-decoration: none;
+  background-color: transparent;
+}
+a:focus,
+a:hover {
+  color: #014c8c;
+  text-decoration: underline;
+  text-decoration: none;
+}
+a:focus {
+  outline: thin dotted;
+  outline-offset: -2px;
+}
 .container {
   margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   text-align: center;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.hero {
+  padding: 3rem 1.5rem;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.love {
+  margin-bottom: 2rem;
 }
-
-.links {
-  padding-top: 15px;
+h1 {
+  font-size: 2.5rem;
+  margin-bottom: 2rem;
+}
+h2 {
+  font-size: 1.6rem;
+  font-weight: 300;
+}
+small {
+  margin-top: 0;
+}
+.btn {
+  overflow: visible;
+  text-transform: uppercase;
+  text-decoration: none;
+  margin: 1em 0;
+  -moz-user-select: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  padding: 0.375rem 1rem;
+  text-align: center;
+  vertical-align: middle;
+  white-space: nowrap;
+  background-color: #0275d8;
+  border-color: #0275d8;
+  color: #fff;
+  border-radius: 0.3rem;
+  font-size: 1.25rem;
+  line-height: 1.33333;
+  padding: 0.75rem 1.25rem;
+}
+.btn:hover,
+.btn:focus,
+.btn:active {
+  background-color: #025aa5;
+  border-color: #01549b;
+  color: #fff;
+  background-image: none;
+}
+.btn:active:focus,
+.btn:active:hover {
+  background-color: #014682;
+  border-color: #01315a;
+  color: #fff;
+}
+.scalingo-logo {
+  max-width: 100%;
+  height: auto;
+  width: 330px;
+}
+.heart-logo {
+  margin: 0 20px;
+  width: 56px;
+  position: relative;
+  top: -18px;
+}
+.tech-logo {
+  width: 65px;
+  height: 65px;
+}
+@media (min-width: 768px) {
+  .container {
+    width: 800px;
+  }
+  .one-liner {
+    display: block;
+  }
+}
+@media (max-width: 767px) {
+  .scalingo-logo,
+  .heart-logo,
+  .tech-logo {
+    display: block;
+    position: unset;
+    margin: 0 auto;
+  }
+  .scalingo-logo,
+  .heart-logo {
+    margin-bottom: 1rem;
+  }
 }
 </style>
